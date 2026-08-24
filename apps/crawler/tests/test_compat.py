2990d53e3c80dcc062c6dcf692556c37c647d674
Viktor Shcherbakov <viktoroo.sch@gmail.com>
2026-08-24 10:22:05 +0200

    Fix Lucca review gates

--- apps/crawler/tests/test_compat.py
+++ apps/crawler/tests/test_compat.py
@@ -74,9 +74,7 @@ def test_detect_dualoo_portal_as_dom():
 
 
 def test_detect_lucca_listing_as_dom():
-    assert (
-        detect_ats_from_url("https://jobs.world.luccasoftware.com/world-aquatics") == "dom"
-    )
+    assert detect_ats_from_url("https://jobs.world.luccasoftware.com/world-aquatics") == "dom"
     assert (
         detect_ats_from_url(
             "https://jobs.world.luccasoftware.com/world-aquatics/"
